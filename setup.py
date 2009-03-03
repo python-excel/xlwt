7835b9f0cd352b7871744d69e57c227a402fb913
sjmachin <sjmachin@ac150577-6901-3246-a117-cd8547e6756f>
2009-03-03 13:11:22 +0000

    Tidy-up and version changes for release 0.7.1
    git-svn-id: https://secure.simplistix.co.uk/svn/xlwt/trunk@3788 ac150577-6901-3246-a117-cd8547e6756f

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
-__rev_id__ = """$Id$"""
-
 import sys, glob
 from distutils.core import setup
 
 DESCRIPTION = (
     'Library to create spreadsheet files compatible with '
     'MS Excel 97/2000/XP/2003 XLS files, '
-    'on any platform, with Python 2.3 or later'
+    'on any platform, with Python 2.3 to 2.6'
     )
 
 LONG_DESCRIPTION = """\
@@ -16,7 +14,7 @@ xlwt is a library for generating spreadsheet files that are compatible
 with Excel 97/2000/XP/2003, OpenOffice.org Calc, and Gnumeric. xlwt has
 full support for Unicode. Excel spreadsheets can be generated on any
 platform without needing Excel or a COM server. The only requirement is
-Python 2.3 or higher. xlwt is a fork of pyExcelerator.
+Python 2.3 to 2.6. xlwt is a fork of pyExcelerator.
 """
 
 CLASSIFIERS = [
@@ -41,7 +39,7 @@ DOWNLOAD_URL = 'http://pypi.python.org/pypi/xlwt'
 
 setup(
     name = 'xlwt',
-    version = '0.7.0',
+    version = '0.7.1',
     maintainer = 'John Machin',
     maintainer_email = 'sjmachin@lexicon.net',
     url = SVN_URL,
