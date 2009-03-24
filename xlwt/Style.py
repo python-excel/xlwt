c591a619b04b2e42e8aabf45ce6d48c7642a54e2
sjmachin <sjmachin@ac150577-6901-3246-a117-cd8547e6756f>
2009-03-24 09:31:39 +0000

    Added .../doc/pattern_examples.xls and put a pointer to it in the easyxf part of Style.py.
    git-svn-id: https://secure.simplistix.co.uk/svn/xlwt/trunk@3816 ac150577-6901-3246-a117-cd8547e6756f

--- xlwt/Style.py
+++ xlwt/Style.py
@@ -347,6 +347,7 @@ del _colour_map_text, _line, _name, _num
 
 pattern_map = {
     # Text values for pattern.pattern
+    # xlwt/doc/pattern_examples.xls showcases all of these patterns.
     'no_fill':              0,
     'none':                 0,
     'solid':                1,
