f3b0aea23fc0480af8fa1b70625da86adf93a9f0
sjmachin <sjmachin@ac150577-6901-3246-a117-cd8547e6756f>
2010-12-20 10:59:49 +0000

    Examples: Renamed numbers.py to numbers_demo.py to avoid clashes with the Python-supplied numbers module. Tidied up big-35Mb.py.
    git-svn-id: https://secure.simplistix.co.uk/svn/xlwt/trunk@4567 ac150577-6901-3246-a117-cd8547e6756f

--- xlwt/examples/big-35Mb.py
+++ xlwt/examples/big-35Mb.py
@@ -13,14 +13,10 @@ colcount = 200 + 1
 rowcount = 6000 + 1
 
 t0 = time()
-print "\nstart: %s" % ctime(t0)
 
-print "Filling..."
 for col in xrange(colcount):
-    print "[%d]" % col, 
     for row in xrange(rowcount):
         ws0.write(row, col, "BIG(%d, %d)" % (row, col))
-        #ws0.write(row, col, "BIG")
 
 t1 = time() - t0
 print "\nsince starting elapsed %.2f s" % (t1)
