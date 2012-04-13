b768184c7ad9a427a0fa67cd9b913aa30321285f
Chris Withers <cwithers@batstrading.com>
2012-04-13 10:49:21 +0100

    We don't need to mention this in the setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ xlwt is a library for generating spreadsheet files that are compatible
 with Excel 97/2000/XP/2003, OpenOffice.org Calc, and Gnumeric. xlwt has
 full support for Unicode. Excel spreadsheets can be generated on any
 platform without needing Excel or a COM server. The only requirement is
-Python 2.3 to 2.7. xlwt is a fork of pyExcelerator.
+Python 2.3 to 2.7.
 """
 
 CLASSIFIERS = [
