be268c8a01f1738c39aa3486802972ae6c7f34d1
Chris Withers <chris@simplistix.co.uk>
2012-06-29 20:53:30 +0100

    Sort out licensing relating to Utils.py.
    Closes GitHub Issue #4.

--- licences.py
+++ licences.py
@@ -80,3 +80,35 @@ Libknecht St., 4
 <roman@kiseliov.ru>
 Subject: pyExcelerator
 """
+
+"""
+Portions of xlwt.Utils based on:
+pyXLWriter - A library for generating Excel Spreadsheets
+
+The licensing of pyXLWriter is as follows:
+
+ Copyright (c) 2004 Evgeny Filatov <fufff@users.sourceforge.net>
+ Copyright (c) 2002-2004 John McNamara (Perl Spreadsheet::WriteExcel)
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; either version 2.1 of the License, or
+ (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ General Public License for more details:
+
+ http://www.gnu.org/licenses/lgpl.html
+
+ pyXLWriter also makes reference to the PERL Spreadsheet::WriteExcel as follows:
+ 
+ ----------------------------------------------------------------------------
+  This module was written/ported from PERL Spreadsheet::WriteExcel module
+  The author of the PERL Spreadsheet::WriteExcel module is John McNamara
+  <jmcnamara@cpan.org>
+ ----------------------------------------------------------------------------
+
+"""
+
--- xlwt/Utils.py
+++ xlwt/Utils.py
@@ -1,29 +1,4 @@
-# pyXLWriter: A library for generating Excel Spreadsheets
-# Copyright (c) 2004 Evgeny Filatov <fufff@users.sourceforge.net>
-# Copyright (c) 2002-2004 John McNamara (Perl Spreadsheet::WriteExcel)
-#
-# This library is free software; you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation; either version 2.1 of the License, or
-# (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
-# General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this library; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
-#----------------------------------------------------------------------------
-# This module was written/ported from PERL Spreadsheet::WriteExcel module
-# The author of the PERL Spreadsheet::WriteExcel module is John McNamara
-# <jmcnamara@cpan.org>
-#----------------------------------------------------------------------------
-# See the README.txt distributed with pyXLWriter for more details.
-
-# Portions are (C) Roman V. Kiseliov, 2005
-
+# see the xlwt.license module for details of licensing.
 
 # Utilities for work with reference to cells and with sheetnames
 
