1f0bddac001959bb94f27c956b7f4f3e93c32de7
Chris Withers <chris@simplistix.co.uk>
2016-06-05 23:54:25 -0500

    tag 3.5 support in setup.py.

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     ]
 
 KEYWORDS = (
