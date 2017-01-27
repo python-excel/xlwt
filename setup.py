ccd40879efd162703432a2ca9f036e478163cb9c
Jon Dufresne <jon.dufresne@gmail.com>
2017-01-26 21:03:55 -0800

    Add Python 3.6 to list of tested Pythons

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     ]
 
 KEYWORDS = (
