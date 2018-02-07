19addb35d54c19e551f5e508eb4bd06777f9162d
Jon Dufresne <jon.dufresne@gmail.com>
2018-02-06 20:02:13 -0800

    Fix flake8 warning "E111 indentation is not a multiple of four"

--- xlwt/ExcelFormulaParser.py
+++ xlwt/ExcelFormulaParser.py
@@ -13,9 +13,9 @@ _RVAdeltaArea = {"R": 0, "V": 0x20, "A": 0x40, "D": 0}
 
 
 class FormulaParseException(Exception):
-   """
-   An exception indicating that a Formula could not be successfully parsed.
-   """
+    """
+    An exception indicating that a Formula could not be successfully parsed.
+    """
 ### header action <<<
 ### preamble action>>>
 
@@ -382,22 +382,22 @@ class Parser(antlr.LLkParser):
             skiplens = [0] * nc
             skiplens[-1] = 3
             for ic in xrange(nc-1, 0, -1):
-               skiplens[ic-1] = skiplens[ic] + chunklens[ic] + 4
+                skiplens[ic-1] = skiplens[ic] + chunklens[ic] + 4
             jump_pos = [2 * nc + 2]
             for ic in xrange(nc):
-               jump_pos.append(jump_pos[-1] + chunklens[ic] + 4)
+                jump_pos.append(jump_pos[-1] + chunklens[ic] + 4)
             chunk_shift = 2 * nc + 6 # size of tAttrChoose
             for ic in xrange(nc):
-               for refx in xrange(ref_markers[ic], ref_markers[ic+1]):
-                   ref = self.sheet_references[refx]
-                   self.sheet_references[refx] = (ref[0], ref[1], ref[2] + chunk_shift)
-               chunk_shift += 4 # size of tAttrSkip
+                for refx in xrange(ref_markers[ic], ref_markers[ic+1]):
+                    ref = self.sheet_references[refx]
+                    self.sheet_references[refx] = (ref[0], ref[1], ref[2] + chunk_shift)
+                chunk_shift += 4 # size of tAttrSkip
             choose_rpn = []
             choose_rpn.append(struct.pack("<BBH", ptgAttr, 0x04, nc)) # 0x04 is tAttrChoose
             choose_rpn.append(struct.pack("<%dH" % (nc+1), *jump_pos))
             for ic in xrange(nc):
-               choose_rpn.append(rpn_chunks[ic])
-               choose_rpn.append(struct.pack("<BBH", ptgAttr, 0x08, skiplens[ic])) # 0x08 is tAttrSkip
+                choose_rpn.append(rpn_chunks[ic])
+                choose_rpn.append(struct.pack("<BBH", ptgAttr, 0x08, skiplens[ic])) # 0x08 is tAttrSkip
             choose_rpn.append(struct.pack("<BBH", ptgFuncVarV, nc+1, 100)) # 100 is CHOOSE fn
             self.rpn += "".join(choose_rpn)
         elif la1 and la1 in [LP]:
@@ -414,9 +414,9 @@ class Parser(antlr.LLkParser):
                 # print "**int_const", int_tok.text
                 int_value = int(int_tok.text)
                 if int_value <= 65535:
-                   self.rpn += struct.pack("<BH", ptgInt, int_value)
+                    self.rpn += struct.pack("<BH", ptgInt, int_value)
                 else:
-                   self.rpn += struct.pack("<Bd", ptgNum, float(int_value))
+                    self.rpn += struct.pack("<Bd", ptgNum, float(int_value))
             elif (self.LA(1)==REF2D) and (_tokenSet_0.member(self.LA(2))):
                 pass
                 ref2d_tok = self.LT(1)
@@ -490,21 +490,21 @@ class Parser(antlr.LLkParser):
                 self.match(NAME)
                 func_toku = func_tok.text.upper()
                 if func_toku in all_funcs_by_name:
-                   (opcode,
-                   min_argc,
-                   max_argc,
-                   func_type,
-                   arg_type_str) = all_funcs_by_name[func_toku]
-                   arg_type_list = list(arg_type_str)
+                    (opcode,
+                     min_argc,
+                     max_argc,
+                     func_type,
+                     arg_type_str) = all_funcs_by_name[func_toku]
+                    arg_type_list = list(arg_type_str)
                 else:
-                   raise Exception("[formula] unknown function (%s)" % func_tok.text)
+                    raise Exception("[formula] unknown function (%s)" % func_tok.text)
                 # print "**func_tok1 %s %s" % (func_toku, func_type)
                 xcall = opcode < 0
                 if xcall:
-                   # The name of the add-in function is passed as the 1st arg
-                   # of the hidden XCALL function
-                   self.xcall_references.append((func_toku, len(self.rpn) + 1))
-                   self.rpn += struct.pack("<BHHH",
+                    # The name of the add-in function is passed as the 1st arg
+                    # of the hidden XCALL function
+                    self.xcall_references.append((func_toku, len(self.rpn) + 1))
+                    self.rpn += struct.pack("<BHHH",
                        ptgNameXR,
                        0xadde, # ##PATCHME## index to REF entry in EXTERNSHEET record
                        0xefbe, # ##PATCHME## one-based index to EXTERNNAME record
@@ -513,18 +513,18 @@ class Parser(antlr.LLkParser):
                 arg_count=self.expr_list(arg_type_list, min_argc, max_argc)
                 self.match(RP)
                 if arg_count > max_argc or arg_count < min_argc:
-                   raise Exception("%d parameters for function: %s" % (arg_count, func_tok.text))
+                    raise Exception("%d parameters for function: %s" % (arg_count, func_tok.text))
                 if xcall:
-                   func_ptg = ptgFuncVarR + _RVAdelta[func_type]
-                   self.rpn += struct.pack("<2BH", func_ptg, arg_count + 1, 255) # 255 is magic XCALL function
+                    func_ptg = ptgFuncVarR + _RVAdelta[func_type]
+                    self.rpn += struct.pack("<2BH", func_ptg, arg_count + 1, 255) # 255 is magic XCALL function
                 elif min_argc == max_argc:
-                   func_ptg = ptgFuncR + _RVAdelta[func_type]
-                   self.rpn += struct.pack("<BH", func_ptg, opcode)
+                    func_ptg = ptgFuncR + _RVAdelta[func_type]
+                    self.rpn += struct.pack("<BH", func_ptg, opcode)
                 elif arg_count == 1 and func_tok.text.upper() == "SUM":
-                   self.rpn += struct.pack("<BBH", ptgAttr, 0x10, 0) # tAttrSum
+                    self.rpn += struct.pack("<BBH", ptgAttr, 0x10, 0) # tAttrSum
                 else:
-                   func_ptg = ptgFuncVarR + _RVAdelta[func_type]
-                   self.rpn += struct.pack("<2BH", func_ptg, arg_count, opcode)
+                    func_ptg = ptgFuncVarR + _RVAdelta[func_type]
+                    self.rpn += struct.pack("<2BH", func_ptg, arg_count, opcode)
             else:
                 raise antlr.NoViableAltException(self.LT(1), self.getFilename())
 
@@ -577,11 +577,11 @@ class Parser(antlr.LLkParser):
                 if (self.LA(1)==COMMA or self.LA(1)==SEMICOLON):
                     pass
                     if arg_cnt < len(arg_type_list):
-                       arg_type = arg_type_list[arg_cnt]
+                        arg_type = arg_type_list[arg_cnt]
                     else:
-                       arg_type = arg_type_list[-1]
+                        arg_type = arg_type_list[-1]
                     if arg_type == "+":
-                       arg_type = arg_type_list[-2]
+                        arg_type = arg_type_list[-2]
                     # print "**expr_list2[%d] req=%s" % (arg_cnt, arg_type)
                     la1 = self.LA(1)
                     if False:
