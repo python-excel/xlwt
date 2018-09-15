9a44aaa2521cfa26e14bd786a04af08a0258c1ab
Jon Dufresne <jon.dufresne@gmail.com>
2018-09-15 14:22:51 -0700

    Add testing for Python 3.7

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     ]
 
 KEYWORDS = (
